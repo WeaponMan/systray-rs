f31e964abdf3be0f56229a500ec8eae67fd66511
WeaponMan <weaponman@centrum.cz>
2020-07-24 16:32:19 +0200

    update gtk

--- src/api/linux/mod.rs
+++ src/api/linux/mod.rs
@@ -23,16 +23,6 @@ pub struct GtkSystrayApp {
 
 thread_local!(static GTK_STASH: RefCell<Option<GtkSystrayApp>> = RefCell::new(None));
 
-pub struct MenuItemInfo {
-    mid: u32,
-    title: String,
-    tooltip: String,
-    disabled: bool,
-    checked: bool,
-}
-
-type Callback = Box<(Fn(&GtkSystrayApp) -> () + 'static)>;
-
 // Convenience function to clean up thread local unwrapping
 fn run_on_gtk_thread<F>(f: F)
 where
@@ -54,7 +44,7 @@ where
 
 impl GtkSystrayApp {
     pub fn new(event_tx: Sender<SystrayEvent>) -> Result<GtkSystrayApp, Error> {
-        if let Err(e) = gtk::init() {
+        if let Err(_) = gtk::init() {
             return Err(Error::OsError(format!("{}", "Gtk init error!")));
         }
         let mut m = gtk::Menu::new();
@@ -77,7 +67,7 @@ impl GtkSystrayApp {
             .ok();
     }
 
-    pub fn add_menu_separator(&self, item_idx: u32) {
+    pub fn add_menu_separator(&self, _item_idx: u32) {
         //let mut menu_items = self.menu_items.borrow_mut();
         let m = gtk::SeparatorMenuItem::new();
         self.menu.append(&m);
@@ -93,7 +83,7 @@ impl GtkSystrayApp {
             self.menu.show_all();
             return;
         }
-        let m = gtk::MenuItem::new_with_label(item_name);
+        let m = gtk::MenuItem::with_label(item_name);
         self.menu.append(&m);
         m.connect_activate(move |_| {
             run_on_gtk_thread(move |stash: &GtkSystrayApp| {
@@ -121,10 +111,10 @@ impl Window {
             GTK_STASH.with(|stash| match GtkSystrayApp::new(event_tx) {
                 Ok(data) => {
                     (*stash.borrow_mut()) = Some(data);
-                    tx.send(Ok(()));
+                    let _ =  tx.send(Ok(()));
                 }
                 Err(e) => {
-                    tx.send(Err(e));
+                    let _  = tx.send(Err(e));
                     return;
                 }
             });
@@ -161,7 +151,7 @@ impl Window {
         Ok(())
     }
 
-    pub fn set_icon_from_resource(&self, resource: &str) -> Result<(), Error> {
+    pub fn set_icon_from_resource(&self, _resource: &str) -> Result<(), Error> {
         panic!("Not implemented on this platform!");
     }
 
@@ -169,7 +159,7 @@ impl Window {
         Ok(())
     }
 
-    pub fn set_tooltip(&self, tooltip: &str) -> Result<(), Error> {
+    pub fn set_tooltip(&self, _tooltip: &str) -> Result<(), Error> {
         panic!("Not implemented on this platform!");
     }
 
